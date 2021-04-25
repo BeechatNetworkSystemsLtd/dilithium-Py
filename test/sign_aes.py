6ea06f5fcd5c6286a6295a324745f997a9a59899
Beechat Network Systems Ltd <7268708+BeechatNetworkSystemsLtd@users.noreply.github.com>
2021-04-25 02:03:22 +0100

    Update sign_aes.py

--- test/sign_aes.py
+++ test/sign_aes.py
@@ -3,25 +3,25 @@ sys.path.append('../build')
 
 import pydilithium_aes;
 
-message = "example message for verification";
+message = "example message for verification"
 
 # Step 1: Create keys
 pydilithium_aes.pqcrystals_dilithium2_ref_keypair()
 
 print("Sign public key:")
-print(pydilithium.pqcrystals_get_pk())
+print(pydilithium_aes.pqcrystals_get_pk())
 print("")
 print("Sign secret key:")
-print(pydilithium.pqcrystals_get_sk())
+print(pydilithium_aes.pqcrystals_get_sk())
 print("")
 print("Original message:")
 print(message)
 print("")
 f = open('key.public', 'w')
-f.write(pydilithium.pqcrystals_get_pk())
+f.write(pydilithium_aes.pqcrystals_get_pk())
 f.close()
 f = open('key.secret', 'w')
-f.write(pydilithium.pqcrystals_get_sk())
+f.write(pydilithium_aes.pqcrystals_get_sk())
 f.close()
 
 # Step 2: Sign message
@@ -35,7 +35,7 @@ f = open('key.public', 'r')
 pk = f.readline()
 f.close()
 
-pydilithium.pqcrystals_set_pk(pk)
+pydilithium_aes.pqcrystals_set_pk(pk)
 check = pydilithium_aes.pqcrystals_dilithium2_ref_open(signed_message, len(signed_message))
 print("Restored message:")
 print(check)
